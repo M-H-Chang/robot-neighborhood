5b92272306cfd05262a5697f6a0b1646920d1b8a
Min Chang <minchangmhc@gmail.com>
2021-04-02 09:28:34 -0700

    WIP: adding numbers to array.

---
+++ README.md
@@ -0,0 +1,7 @@
+Specs
+
+Describe identifyOne()
+Test: It should return an array of one in the console.
+Code: identifyOne("5641351")
+Expected Output: [1, 1]
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 // -----Utility Logic-----
+const firstNumber = [1]
+const secondNumber = [2]
+const thirdNumber = [3]
 
 // -----Business Logic-----
-
+function identifyOne(number) {
+  const numArray = [];
+  const oneArray = [];
+  numArray.forEach(function (element) {
+    if (numArray === firstNumber) {
+      oneArray.push();
+      console.log(oneArray);
+    }
+  });
+}
 // -----User Interface Logic-----
 $("document").ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
-
+    const num = $("#num").val().numArray.push();
   })
 })
