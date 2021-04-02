9638cb27c29554aef395cc05bebd7027724b72ac
Min Chang <minchangmhc@gmail.com>
2021-04-02 11:36:21 -0700

    change the function so it detects multiples 1's

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Test: If a number is 1 replace it with "Beep!"
 Code identifyOne("1")
 Expected Output: ["Beep!"]
 
+Test: If the number has multiple 1's console log multiple beep's.
+Code identifyOne("12341")
+Expected Output: (2)beep
+
--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,15 @@
 // -----Business Logic-----
 function identifyOne(number) {
   const numArray = number.split("");
-  // for (let i = 0; i < numArray.length; i++) {
-  //   if (numArray[i] === firstNumber) {
-  //     numArray[i].replace(firstNumber, "Beep!");
-  //   } console.log(numArray);
-  // }
-  if (numArray[0] === "1") {
-    numArray.splice(0, 1, "Beep!")
-  } console.log(numArray)
 
-  // numArray.forEach(function (elements) {
-  //   if (elements === "1") {
-  //     numArray.replace("1", "Beep!");
-  //   } console.log(numArray)
-  // });
+
+
+  numArray.forEach(function (element) {
+    if (element === "1") {
+
+      console.log("beep")
+    }
+  });
 }
 // -----User Interface Logic-----
 $("document").ready(function () {
