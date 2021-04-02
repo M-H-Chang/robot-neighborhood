f979e7041738b617383cb5300c7be830276b185b
Min Chang <minchangmhc@gmail.com>
2021-04-02 10:17:06 -0700

    Changed spec to simpler terms and debugged.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,21 @@
 // -----Utility Logic-----
-const firstNumber = [1]
-const secondNumber = [2]
-const thirdNumber = [3]
+const firstNumber = "1";
+const secondNumber = "2";
+const thirdNumber = "3";
 
 // -----Business Logic-----
 function identifyOne(number) {
-  const numArray = [];
-  const oneArray = [];
-  numArray.forEach(function (element) {
-    if (numArray === firstNumber) {
-      oneArray.push();
-      console.log(oneArray);
-    }
-  });
+  const numArray = number.split("");
+  // const oneArray = [];
+  console.log(numArray)
+  // numArray.forEach(function (elements) {
+
+  // });
 }
 // -----User Interface Logic-----
 $("document").ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
-    const num = $("#num").val().numArray.push();
+    const num = parseInt($("#input").val());
   })
 })
